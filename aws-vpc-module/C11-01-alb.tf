827ad52a60df436838a0412e95fa28703285a706
gopal das <gopal1409@gmail.com>
2022-01-18 16:34:34 +0530

    aws devops

---
+++ aws-vpc-module/C11-01-alb.tf
@@ -0,0 +1,13 @@
+/*resource "aws_lb" "web_servers" {
+ name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  #vpc_id = module.vpc.vpc_id 
+  security_groups    = [module.loadbalancer_sg.this_security_group_id]
+  subnets            = [
+      module.vpc.public_subnets[0],
+      module.vpc.public_subnets[1]
+  ]
+
+  enable_deletion_protection = false
+}*/
---
+++ aws-vpc-module/c1-version.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }   
+  }
+  backend "s3" {
+    #change the bucket name
+    bucket = "terraform-on-aws-for-ec2-gopal"
+    key = "dev/terraform.tfstate"
+    region = "us-east-1"
+    #change the dyanmodb table name if you have modified it
+    #for state locking
+    dynamodb_table = "dev-project-vpc"
+  }
+
+  
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
